6d90d4d4b8fc41d52fd1ffbd171ab1a55258a8d9
yana0616 <rainymoment0616@gmail.com>
2026-05-31 13:49:34 +0900

    feat: 作業5日目（詳細ページもprisma化）

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { Post } from "@/types/post";
+import { prisma } from "@/lib/prisma";
+import { notFound } from 'next/navigation';
+// import { Post } from "@/types/post";
 
 import PostDetail from "./_components/PostDetail";
 
@@ -9,8 +11,15 @@ type Props = {
 export default async function PostPage({ params }: Props) {
   const { id } = await params; // TSの分割代入
   console.log(id);
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-  const post: Post = await res.json();
+  // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  // const post: Post = await res.json();
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) }
+  });
+
+  if (!post) {
+    notFound()
+  }
 
   return (
     <main>
